658baec0574f31dbea5d6d4ed841ed70fb7d040a
OlgaMaciaszek <olga.maciaszek@gmail.com>
2018-07-22 19:28:07 +0200

    Remove colons.

--- src/Address.groovy
+++ src/Address.groovy
@@ -4,31 +4,31 @@
  */
 public class Address {
 
-	private String streetAddress;
-	private String postalCode;
-	private String city;
+	private String streetAddress
+	private String postalCode
+	private String city
 
 	public String getStreetAddress() {
-		return streetAddress;
+		return streetAddress
 	}
 
 	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
+		this.streetAddress = streetAddress
 	}
 
 	public String getPostalCode() {
-		return postalCode;
+		return postalCode
 	}
 
 	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
+		this.postalCode = postalCode
 	}
 
 	public String getCity() {
-		return city;
+		return city
 	}
 
 	public void setCity(String city) {
-		this.city = city;
+		this.city = city
 	}
 }
--- src/Person.groovy
+++ src/Person.groovy
@@ -4,67 +4,67 @@
  */
 public class Person {
 
-	private String name;
-	private String surname;
-	private List<String> emails = new ArrayList<>();
-	private Address address;
-	private Integer netWorth;
+	private String name
+	private String surname
+	private List<String> emails = new ArrayList<>()
+	private Address address
+	private Integer netWorth
 
 	public Person() {
 	}
 
 	public Person(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
+		this.name = name
+		this.surname = surname
 	}
 
 	public Person(String name, String surname, List<String> emails) {
-		this(name, surname);
-		this.emails = emails;
+		this(name, surname)
+		this.emails = emails
 	}
 
 	public Integer getNetWorth() {
-		return netWorth != null ? netWorth : 0;
+		return netWorth != null ? netWorth : 0
 	}
 
 	public void setNetWorth(Integer netWorth) {
-		this.netWorth = netWorth;
+		this.netWorth = netWorth
 	}
 
 	public List<String> getEmails() {
-		return emails;
+		return emails
 	}
 
 	public String getFullName() {
-		return getFullName("Ms.");
+		return getFullName("Ms.")
 	}
 
 	public String getFullName(String prefix) {
-		return prefix + " " + name + " " + surname;
+		return prefix + " " + name + " " + surname
 	}
 
 	public void addEmail(String email) {
-		emails.add(email);
+		emails.add(email)
 	}
 
 	public String getCity() {
 		if (address != null) {
-			return address.getCity();
+			return address.getCity()
 		}
-		return null;
+		return null
 	}
 
 	public void printFirstEmail() {
-		System.out.print("Printing first email: ");
+		System.out.print("Printing first email: ")
 		if (emails.size() >= 1) {
-			System.out.println(emails.get(0));
+			System.out.println(emails.get(0))
 		} else {
-			System.out.println("-");
+			System.out.println("-")
 		}
 	}
 
 	public boolean sameEmails(List<String> emails) {
-		return this.emails.equals(emails);
+		return this.emails.equals(emails)
 	}
 
 	@Override
@@ -75,23 +75,23 @@ public class Person {
 				", emails=" + emails +
 				", address=" + address +
 				", netWorth=" + netWorth +
-				'}';
+				'}'
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Person)) return false;
-		Person person = (Person) o;
+		if (this == o) return true
+		if (!(o instanceof Person)) return false
+		Person person = (Person) o
 		return Objects.equals(name, person.name) &&
 				Objects.equals(surname, person.surname) &&
 				Objects.equals(emails, person.emails) &&
 				Objects.equals(address, person.address) &&
-				Objects.equals(netWorth, person.netWorth);
+				Objects.equals(netWorth, person.netWorth)
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(name, surname, emails, address, netWorth);
+		return Objects.hash(name, surname, emails, address, netWorth)
 	}
 }
