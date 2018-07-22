3e35bccdd116ec647df71e3f23f120b9ed01a61c
OlgaMaciaszek <olga.maciaszek@gmail.com>
2018-07-22 21:35:01 +0200

    Clean up Test script.

--- src/Test.groovy
+++ src/Test.groovy
@@ -2,14 +2,15 @@
  * @author Olga Maciaszek-Sharma
  * @since 3/15/18
  */
-Person person = new Person("Anna", "Smith")
-System.out.println(person.getFullName())
-System.out.println("Printing city: " + person.getCity())
-System.out.println("Net worth: " + person.getNetWorth())
-person.setNetWorth(1000000)
-System.out.println("Net worth: " + person.getNetWorth())
+
+Person person = new Person('Anna', 'Smith')
+println person.getFullName()
+println "Printing city: ${person.getCity()}"
+println "Net worth: ${person.netWorth}"
+person.netWorth = 1_000_000
+println "Net worth: ${person.netWorth}"
 person.printFirstEmail()
-person.addEmail("test@test.com")
+person.addEmail 'test@test.com'
 person.printFirstEmail()
-System.out.println(person.toString())
-System.out.println(person.getEmails().equals(Collections.singletonList("test@test.com")))
+println person
+println person.getEmails() == ['test@test.com']
